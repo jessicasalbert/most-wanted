0b87cbab845d54c6ed7f756e461c0c880e7b8ff4
jessicasalbert <jessicarsalbert@gmail.com>
2020-09-28 14:34:56 -0400

    completed seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     belongs_to :city
     has_many :updates
     has_many :crimes, through: :updates
+    has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+require 'faker'
+
 Crime.destroy_all
 City.destroy_all
+User.destroy_all
 
-def seed
+def seed_crimes
     page = 1
     45.times do 
         endpoint = 'https://api.fbi.gov/wanted/v1/list?page=' + page.to_s
@@ -10,14 +13,14 @@ def seed
         data['items'].each do |crime|
             if crime['field_offices']
                 city = City.find_by(name: crime['field_offices'][0])
-                Crime.create(title: crime['title'], description: crime['description'], race_raw: crime['race_raw'], eyes: crime['eyes'], hair_raw: crime['hair_raw'], details: crime['details'], birthday: crime['dates_of_birth_used'], image: crime['images'][0]['large'], city: city)
+                Crime.create(title: crime['title'], description: crime['description'], race_raw: crime['race_raw'], eyes: crime['eyes'], hair_raw: crime['hair_raw'], details: crime['details'], birthday: crime['dates_of_birth_used'], image: crime['images'][0]['large'], image_small: crime['images'][0]['thumb'], city: city, warning_message: crime['warning_message'], scars_and_marks: crime['scars_and_marks'], alias: crime['aliases'], caution: crime['caution'], sex: crime['sex'], url: crime['url'], remarks: crime['remarks'], reward: crime['reward'])
             end
         end
         page += 1
     end
 end
 
-def cities
+def seed_cities
     cities = []
     page = 1
     45.times do 
@@ -36,19 +39,29 @@ def cities
     end
 end
 
-def seed_sample
-    endpoint = 'https://api.fbi.gov/wanted/v1/list?page='
-    data = Net::HTTP.get(URI(endpoint))
-    data = JSON.parse(data)
-    data['items'].each do |crime|
-        city = City.find_by(name: crime['field_offices'][0])
-        Crime.create(title: crime['title'], description: crime['description'], race_raw: crime['race_raw'], eyes: crime['eyes'], hair_raw: crime['hair_raw'], details: crime['details'], birthday: crime['dates_of_birth_used'], image: crime['images'][0]['large'], location_id: city.id)
-    end
+seed_cities
+seed_crimes
+
+
+10.times do 
+    User.create(name: Faker::Name.name, username: Faker::Internet.email, password: "abc123", city: City.all.sample, badge_id: rand(1000000...9999999))
 end
 
 
 
 
-cities
-seed
+
+# def seed_sample
+#     endpoint = 'https://api.fbi.gov/wanted/v1/list?page='
+#     data = Net::HTTP.get(URI(endpoint))
+#     data = JSON.parse(data)
+#     data['items'].each do |crime|
+#         city = City.find_by(name: crime['field_offices'][0])
+#         Crime.create(title: crime['title'], description: crime['description'], race_raw: crime['race_raw'], eyes: crime['eyes'], hair_raw: crime['hair_raw'], details: crime['details'], birthday: crime['dates_of_birth_used'], image: crime['images'][0]['large'], location_id: city.id)
+#     end
+# end
+
+
+
+
 
