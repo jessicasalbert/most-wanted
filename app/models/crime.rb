fc7517f5726b2c87a6a71f42f77a0a06b599584f
jessicasalbert <jessicarsalbert@gmail.com>
2020-09-29 17:58:42 -0400

    added crime methods

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -5,4 +5,16 @@ class Crime < ApplicationRecord
     has_many :anon_tips
 
 
+    def self.random_crimes
+        randoms = []
+        5.times do 
+            randoms << Crime.all.sample
+        end
+        randoms
+    end
+
+    def self.five_most_wanted
+        Crime.all.order(reward: :desc).limit(5)
+    end
+
 end
